9c39672939bcadef069921ee5e9571ef09591429
Гаврилова Иван <discover1977@yandex.ru>
2016-11-03 13:58:56 +0300

    Add main.c

---
+++ main.c
@@ -0,0 +1,26 @@
+/*
+ * main.c
+ *
+ *  Created on: 3 но€б. 2016 г.
+ *      Author: gavrilov.iv
+ */
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+#include "bits_macros.h"
+#include "i2c-soft.h"
+#include "ssd1306.h"
+#include "buttons.h"
+#include "ws2812b/ws2812.h"
+
+
+int main() {
+
+
+
+	while(1) {
+
+	}
+	return 0;
+}
