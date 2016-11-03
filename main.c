5eb314cb3338510fd371a35dcd7527dc11694d30
Гаврилова Иван <discover1977@yandex.ru>
2016-11-03 15:45:21 +0300

    test

--- main.c
+++ main.c
@@ -17,10 +17,30 @@
 
 int main() {
 
+	SetBit(DDRC, 1);
+	ClearBit(PORTC, 1);
 
+	WS2812Init();
 
 	while(1) {
-
+		for (int j = 0; j < 256; j++) {
+			for (int i = 0; i < 256; i++) {
+				//WS2812SetRGB(i, 255, 255, 255);
+				WS2812SetHSV(i, 1530, 0, j);
+			}
+			WS2812Write(colors, LED_COUNT);
+			_delay_ms(10);
+		}
+		_delay_ms(250);
+		for (int j = 255; j > -1; j--) {
+			for (int i = 0; i < 256; i++) {
+				//WS2812SetRGB(i, 255, 255, 255);
+				WS2812SetHSV(i, 1530, 0, j);
+			}
+			WS2812Write(colors, LED_COUNT);
+			_delay_ms(10);
+		}
+		_delay_ms(250);
 	}
 	return 0;
 }
