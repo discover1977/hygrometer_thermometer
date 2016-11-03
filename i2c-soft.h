54ab14ff16f38b20f0c210fcc4c9099b5d62bbff
Гаврилова Иван <discover1977@yandex.ru>
2016-11-03 13:46:13 +0300

    Add any projects files

---
+++ i2c-soft.h
@@ -0,0 +1,41 @@
+/*
+ * i2c-soft.h
+ *
+ *  Created on: 19.12.2009
+ *      Author: Pavel V. Gololobov
+ */
+
+#ifndef I2C_H_
+#define I2C_H_
+
+#define NO_I2C_ACK 0
+#define OK_I2C_ACK 1
+
+#ifndef SDA
+#define I2COUT      PORTB	// Write to Port
+#define I2CIN       PINB	// Read from Port
+#define I2CDIR      DDRB	// Set Port Direction
+#define I2CSEL      PORTB	// Alternative Port Functions
+
+#define SCL       	0b00000001		// Serial Clock Line
+#define SDA       	0b00000010		// Serial Data Line
+
+#endif
+
+// Init Bus
+void i2c_Soft_Init();
+// Start Transfer
+void i2c_Soft_Start();
+// Stop Transfer
+void i2c_Soft_Stop();
+// Write Transfer
+unsigned int i2c_Soft_Write(unsigned int a);
+// Read Transfer
+unsigned int i2c_Soft_Read(unsigned int ack);
+
+// Read Byte
+unsigned int i2c_Soft_ReadByte(unsigned int nAddress, unsigned int nRegister);
+// Write Byte
+void i2c_Soft_WriteByte(unsigned int nAddress, unsigned int nRegister, unsigned int nValue);
+
+#endif
